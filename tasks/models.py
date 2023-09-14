1e3103f928cf548af2cf25926f47031f888dea6f
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-09-14 15:07:54 -0300

    Task Model and Migration

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS = (
+        (True, 'Done'),
+        (False, 'Doing')
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.BooleanField(
+        choices=STATUS,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
