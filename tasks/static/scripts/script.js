cd1dc705344c46b7befe2fe4383e4b1b0744fafd
montenegroleticia <montenegro.letiicia@gmail.com>
2023-09-22 16:14:12 -0300

    Pagination

--- tasks/static/scripts/script.js
+++ tasks/static/scripts/script.js
@@ -1,1 +1,15 @@
-console.log("Funciona!");
+$(document).ready(function () {
+    var deleteBtn = $('.delete-btn');
+
+    $(deleteBtn).on('click', function (e) {
+
+        e.preventDefault();
+
+        var deleteLink = $(this).attr('href');
+        var result = confirm('Do you want to delete this task?');
+
+        if (result) {
+            window.location.href = deleteLink;
+        }
+    });
+})
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
@@ -10,7 +11,13 @@ def health(request):
 
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 3)
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
+
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 
