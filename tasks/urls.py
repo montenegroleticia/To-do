fa407a1bd9b34aedad74cf20680661be3cb5479d
montenegroleticia <montenegro.letiicia@gmail.com>
2023-09-22 15:04:58 -0300

    Delete Task

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.taskList, name='task-list'),
     path('task/<int:id>', views.taskView, name='task-view'),
     path('newtask/', views.newTask, name="new-task"),
+    path('delete/<int:id>', views.deleteTask, name='delete-task'),
     path('edit/<int:id>', views.editTask, name='edit-task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 
 def health(request):
@@ -46,3 +47,12 @@ def editTask(request, id):
             return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
     else:
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(request, 'Task deleted with sucess')
+
+    return redirect('/')
